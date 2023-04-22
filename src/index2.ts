f11c3ab1b6a8a4cd56ebf9b87f1a643752fd6f09
herasake <ogawa.ogatatsu.tatsuya.jp@gmail.com>
2023-04-22 20:49:00 +0900

    2023/04/22 20:49

---
+++ src/index2.ts
@@ -0,0 +1,3 @@
+import { name555, age555, uhyoName, age556 as abcName } from "./uhyo.js";
+
+console.log(name555, age555, uhyoName, abcName);
---
+++ src/uhyo.ts
@@ -0,0 +1,7 @@
+export const name555 = "uhyo";
+export const age555 = 26;
+
+const name556 = "uhyo2";
+const age556 = 27;
+
+export { name556 as uhyoName, age556 };
